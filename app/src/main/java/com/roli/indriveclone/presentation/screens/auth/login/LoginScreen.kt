08f6d2c6035f071e26155a5c015151e03938af11
Rolando <rolyan66@gmail.com>
2026-05-19 22:30:43 -0400

    Se realizaron la adicion de nuevos campos para el login

--- app/src/main/java/com/roli/indriveclone/presentation/screens/auth/login/LoginScreen.kt
+++ app/src/main/java/com/roli/indriveclone/presentation/screens/auth/login/LoginScreen.kt
@@ -5,19 +5,28 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -31,6 +40,12 @@ import com.roli.indriveclone.R
 
 @Composable
 fun LoginScreen() {
+    var email by remember{
+        mutableStateOf("")
+    }
+    var password by remember{
+        mutableStateOf("")
+    }
     Scaffold(
         contentWindowInsets = WindowInsets.navigationBars
     ){paddingValues ->
@@ -103,6 +118,63 @@ fun LoginScreen() {
                         fontSize = 27.sp,
                         fontWeight = FontWeight.Bold
                     )
+                    TextField(
+                        value =email,
+                        onValueChange={
+                            email=it
+                        },
+                        label = {(Text(text = "Email"))}
+                    )
+                    TextField(
+                        value =password,
+                        onValueChange={
+                            password=it
+                        },
+                        label = {(Text(text = "Password"))}
+                    )
+                    Button(
+                        onClick = {}
+                    ) {
+                        Text(text = "Iniciar Sesión")
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Spacer(
+                            modifier = Modifier
+                                .width(25.dp)
+                                .height(1.dp)
+                                .background(Color.White)
+                        )
+                        Text(
+                            text = "O",
+                            color=Color.White,
+                            fontSize = 17.sp
+                        )
+                        Spacer(
+                            modifier = Modifier
+                                .width(25.dp)
+                                .height(1.dp)
+                                .background(Color.White)
+                        )
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                         Text(
+                            text = "No tienes cuenta? ",
+                            color=Color.White,
+                            fontSize = 17.sp
+                        )
+                        Text(
+                            text = "Registrate",
+                            color=Color.White,
+                            fontSize = 17.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
                 }
             }
         }
